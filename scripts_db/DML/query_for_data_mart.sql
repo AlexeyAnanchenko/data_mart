a688590f080fc5086a3aa36966eb8b11d1107739
alexey_ananchenko <ananchenko_as@mail.ru>
2023-07-04 10:23:30 +0300

    обновил схему БД c промежуточными продажами

--- scripts_db/DML/query_for_data_mart.sql
+++ scripts_db/DML/query_for_data_mart.sql
@@ -1,9 +1,17 @@
 WITH
-sales_new AS (
+sales_total AS (
+	SELECT product_id, sales_date, sales_cnt, (SELECT DISTINCT shop_id FROM shop WHERE shop_name = 'DNS')
+	FROM shop_dns
+	UNION SELECT product_id, sales_date, sales_cnt, (SELECT DISTINCT shop_id FROM shop WHERE shop_name = 'М.Видео')
+		  FROM shop_mvideo
+	UNION SELECT product_id, sales_date, sales_cnt, (SELECT DISTINCT shop_id FROM shop WHERE shop_name = 'Ситилинк')
+		  FROM shop_citilink
+),
+sales AS (
 	SELECT s.shop_id, s.product_id, s.last_date_of_month, SUM(s.sales_cnt) AS sales_of_month
 	FROM (SELECT shop_id, product_id, sales_cnt, 
 		(date_trunc('month', sales_date::date) + interval '1 month' - interval '1 day')::date AS last_date_of_month
-		FROM sales) AS s
+		FROM sales_total) AS s
 	GROUP BY shop_id, product_id, last_date_of_month
 	ORDER BY shop_id, product_id
 )
@@ -19,5 +27,5 @@ SELECT sh.shop_name,
 	   ROUND(((s.sales_of_month * pr.price) / (p.plan_cnt * pr.price)), 2) AS "income_fact/income_plan"
 FROM plan AS p
 JOIN shop AS sh ON p.shop_id = sh.shop_id
-JOIN sales_new AS s ON p.shop_id = s.shop_id AND p.product_id = s.product_id AND p.plan_date = s.last_date_of_month
+JOIN sales AS s ON p.shop_id = s.shop_id AND p.product_id = s.product_id AND p.plan_date = s.last_date_of_month
 JOIN products AS pr ON p.product_id = pr.product_id;
